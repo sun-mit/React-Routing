cf9f8d92cd1c9a16ee5c7fe58d77a2ea4ba859a5
sun-mit <amitroysunjoy@gmail.com>
2021-05-20 14:00:51 +0600

    Routing update

---
+++ src/ItemDetail.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import { Link } from 'react-router-dom'
+
+function ItemDetail({match}) {
+
+    useEffect(() => {
+        fetchItem();
+        console.log(match)
+    }, []);
+    
+    const [item, setItem] = useState([]);
+
+    const fetchItem = async () => {
+        const fetchItem = await fetch('https://fortnite-api.theapinetwork.com/item/get?id=${match.params.id}'
+        );
+        const item = await fetchItem.json();
+        setItem(item);
+        console.log(item);
+
+    };
+    
+   
+  return (
+    <div>
+       
+            <h1>{item.name}</h1>
+    </div>
+  );
+}
+
+export default ItemDetail;
--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { Link } from 'react-router-dom'
 
 function Shop() {
+
+    useEffect(() => {
+       fetchItems();
+        
+    }, []);
+    const [items, setItems] = useState([]);
+
+const fetchItems = async () => {
+    const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get'
+    );
+    const items = await data.json();
+    console.log(items.data);
+    setItems(items.data);
+
+};
+
   return (
     <div>
-        <h1>Shop Page</h1>
+    {items.map(item => (
+
+        <h1 key={item.itemid}>
+        <Link to={`shop/${item.name}`}>
+
+        {item.item.name}
+
+        </Link>
+
+        </h1>
+    ))}
+       
+            
     </div>
   );
 }
